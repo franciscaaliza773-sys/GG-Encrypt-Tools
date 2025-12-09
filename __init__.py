5ae725fe93e258e4a68b26caf55976fd4747ff2c
franciscaaliza773-sys <franciscaaliza773@gmail.com>
2025-12-09 12:24:23 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,6 @@
+# __init__.py
+# GG IMGEncrypt 自定义节点插件入口
+
+from .GG_IMGEncrypt import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
